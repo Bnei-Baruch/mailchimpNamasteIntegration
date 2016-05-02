64abad0ff476f82165d44524fe6ec8c647a2b859
davgur <davgur.ru@gmail.com>
2016-05-02 19:51:11 +0300

    add enroll button for logined user in lading page

--- form-shortcode/renderHtml.php
+++ form-shortcode/renderHtml.php
@@ -2,9 +2,16 @@
 function registerForm_func($args) {
 	$isAdmin = wp_get_current_user ()->roles [0] == "administrator";
 	
-	if (is_user_logged_in () && ! $isAdmin)
-		return '<h1>' . __ ( 'The user is logged in.', 'cfef' ) . '</h1>';
-	elseif ($isAdmin) {
+	if (is_user_logged_in () && ! $isAdmin) {		
+		if (isset ( $args ['enrollto'] )) {
+			$enrollArgs = array (
+					course_id => $args ['enrollto'] 
+			);			
+			return NamasteLMSShortcodesController::enroll ( $enrollArgs );
+		} else{
+			return '<h1>' . __ ( 'The user is logged in.', 'cfef' ) . '</h1>';
+		}
+	} elseif ($isAdmin) {
 		return '<a href="" id="registerUsersFromExel" class="button medium submit">' . __ ( 'Register', 'cfef' ) . ' From EXEL</a></form>';
 	}
 	$fieldsId = get_option ( 'mailChimpFieldList' );
@@ -70,29 +77,28 @@ function registerForm_func($args) {
 	return $str;
 }
 function loginForm_func($args) {
-	
-	$username = isset($_COOKIE['username']) ? $_COOKIE['username'] : "";;
-	//$password = isset($_COOKIE['password']) ? $_COOKIE['password'] : "";
-	$forgetmenot = isset($_COOKIE['username']) ? " checked=\"checked\"" : "";
+	$username = isset ( $_COOKIE ['username'] ) ? $_COOKIE ['username'] : "";
+	// $password = isset($_COOKIE['password']) ? $_COOKIE['password'] : "";
+	$forgetmenot = isset ( $_COOKIE ['username'] ) ? " checked=\"checked\"" : "";
 	
 	if (is_user_logged_in ()) {
 		return '<h1>' . __ ( 'The user is logged in.', 'cfef' ) . '</h1>';
 	}
-		
+	
 	$str = '<form class="lr-form" id="loginForm" actoin="#" metod="post">';
 	$str .= '<div class="preloader"></div>';
 	$str .= '<div class="errorMsg" style="color: red;display: none;"></div>';
 	
 	$str .= '<div class="form-field">
 	            <label for="user_login">' . __ ( 'Email', 'cfef' ) . ' <strong>*</strong></label>
-	            <input id="user_login" type="email" name="user_login" placeholder="' . __ ( 'Email', 'cfef' ) . '" value="'. $username .'" required autocomplete="on">
+	            <input id="user_login" type="email" name="user_login" placeholder="' . __ ( 'Email', 'cfef' ) . '" value="' . $username . '" required autocomplete="on">
 	        </div>';
 	$str .= '<div class="form-field">
 	            <label for="user_pass">' . __ ( 'Password', 'cfef' ) . ' <strong>*</strong></label>
 	            <input id="user_pass" type="password" name="user_pass" placeholder="' . __ ( 'Password', 'cfef' ) . '" required autocomplete="on">
 	        </div>';
 	$str .= '<div class="rememberMe">
-				<input name="rememberme" id="rememberme"  type="checkbox" '. $forgetmenot .' />
+				<input name="rememberme" id="rememberme"  type="checkbox" ' . $forgetmenot . ' />
 				<label for="rememberme">' . __ ( 'Remember Me' ) . '</label>
        			<input type="hidden" name="redirect_to" value="' . $_SERVER ['REQUEST_URI'] . '" >
     		</div>';
